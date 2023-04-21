a41ae7d94a94e9ac871b2a1f46d8b5e9dca08ac7
bluelily555 <bluelily555@daum.net>
2023-04-21 11:17:53 +0900

    test

---
+++ java-project2/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import week1.day5.Codeup1017;
+
+public class Main {
+
+    public static void main(String[] args){
+        Codeup1017 code = new Codeup1017();
+
+        code.problem();
+    }
+}
---
+++ java-project2/src/main/java/week1/day5/Codeup1017.java
@@ -0,0 +1,13 @@
+package week1.day5;
+
+import java.util.Scanner;
+
+public class Codeup1017 {
+    public void problem(){
+        Scanner sc = new Scanner(System.in);
+
+        int i = sc.nextInt();
+
+        System.out.println(i+" "+i+" "+i);
+    }
+}
