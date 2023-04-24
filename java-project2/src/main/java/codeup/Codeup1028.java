9d72069308dbfbcbd25af5b7e0c1c4d6b0f1241d
bluelily555 <bluelily555@daum.net>
2023-04-24 15:41:08 +0900

    week2_day1 - codeup 1028, 1029, 1030

---
+++ java-project2/src/main/java/codeup/Codeup1028.java
@@ -0,0 +1,13 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Codeup1028 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long lInteger = sc.nextLong();
+
+        System.out.println(lInteger);
+    }
+}
---
+++ java-project2/src/main/java/codeup/Codeup1030.java
@@ -0,0 +1,14 @@
+package codeup;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Codeup1030 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigDecimal bd = sc.nextBigDecimal();
+
+        System.out.println(bd);
+
+    }
+}
