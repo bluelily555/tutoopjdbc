bedefd536678ee85260302ee46c7c89696b126c4
bluelily555 <bluelily555@daum.net>
2023-04-25 16:06:13 +0900

    week2_day2 - Java Example

---
+++ java-project2/src/main/java/week2/day2/ReferenceTypeArray.java
@@ -0,0 +1,13 @@
+package week2.day2;
+
+import java.util.List;
+
+public class ReferenceTypeArray {
+    public static void main(String[] args) {
+        String[] sArr = new String[3];
+        System.out.println(sArr[0]);
+
+        List[] listArr = new List[3];
+        System.out.println(listArr[0]);
+    }
+}
