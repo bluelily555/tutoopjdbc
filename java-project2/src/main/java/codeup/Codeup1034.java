178cd9cee88e0f06b6420775d3914803e6138248
bluelily555 <bluelily555@daum.net>
2023-04-25 10:47:37 +0900

    week2_day2 - Codeup 1034 수정

--- java-project2/src/main/java/codeup/Codeup1034.java
+++ java-project2/src/main/java/codeup/Codeup1034.java
@@ -5,17 +5,8 @@ import java.util.Scanner;
 public class Codeup1034 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        long octal = sc.nextLong();
-        long decimal = 0;
-        int i = 1;
+        int octal = sc.nextInt(8);
 
-        while(octal > 0){
-            decimal = decimal + (octal % 10 * i);
-            octal /= 10;
-
-            i *= 8;
-        }
-
-        System.out.println(decimal);
+        System.out.printf("%d", octal);
     }
 }
