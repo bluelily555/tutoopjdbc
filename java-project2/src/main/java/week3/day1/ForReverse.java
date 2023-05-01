048011651fa06b951154aa15d1b2693e6838d8b4
bluelily555 <bluelily555@daum.net>
2023-05-01 11:34:25 +0900

    week3_day1 - Java Example for문 reverse 3종

--- java-project2/src/main/java/week3/day1/ForReverse.java
+++ java-project2/src/main/java/week3/day1/ForReverse.java
@@ -2,9 +2,13 @@ package week3.day1;
 
 public class ForReverse {
     public static void main(String[] args) {
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+        for (int i = 10; i > 0; i--) {
+            System.out.printf("%d ", i);
+        }
         for (int i = 10; i >= 0; i--) {
-//            System.out.println(i);
-            
             System.out.printf("%d ", i);
         }
     }
