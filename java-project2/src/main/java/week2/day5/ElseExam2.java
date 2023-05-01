efbdab912520813ac360ba5b8360bd876523c621
bluelily555 <bluelily555@daum.net>
2023-05-01 09:46:06 +0900

    week2_day5 - Java Example ElseExam2 출력 수정

--- java-project2/src/main/java/week2/day5/ElseExam2.java
+++ java-project2/src/main/java/week2/day5/ElseExam2.java
@@ -6,7 +6,7 @@ public class ElseExam2 {
         if (account >= 1500) {
             System.out.println("잔액을 차감합니다.");
         } else {
-            System.out.println("'잔액이 부족합니다.'를 재생합니다.");
+            System.out.println("잔액이 부족합니다.");
         }
     }
 }
