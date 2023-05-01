fd8113e304e94a9ea28ee646ad0e59c278d1270e
bluelily555 <bluelily555@daum.net>
2023-05-01 11:49:35 +0900

    week3_day1 - Codeup 1073~1075

---
+++ java-project2/src/main/java/codeup/Codeup1075.java
@@ -0,0 +1,14 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Codeup1075 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+
+        while(input > 0){
+            System.out.println(input--);
+        }
+    }
+}
