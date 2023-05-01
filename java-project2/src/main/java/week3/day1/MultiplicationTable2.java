a4174be3b02a595ba1f16ced9e9707e950d084f5
bluelily555 <bluelily555@daum.net>
2023-05-01 14:29:25 +0900

    Java Example 구구단 2단, 구구단 2~4단 (week3_day1)

---
+++ java-project2/src/main/java/week3/day1/MultiplicationTable2.java
@@ -0,0 +1,18 @@
+package week3.day1;
+
+public class MultiplicationTable2 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++){
+            System.out.printf("2 * %d = %d\n", i, 2 * i);
+
+        }
+        for (int i = 1; i < 10; i++){
+            System.out.printf("3 * %d = %d\n", i, 3 * i);
+
+        }
+        for (int i = 1; i < 10; i++){
+            System.out.printf("4 * %d = %d\n", i, 4 * i);
+
+        }
+    }
+}
