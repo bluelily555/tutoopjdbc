b4c7d6f203bb154c310881242f133f095647c9e7
bluelily555 <bluelily555@daum.net>
2023-05-01 14:43:39 +0900

    도전과제 구구단 2~4단 메서드 출력 (week3_day1)

--- java-project2/src/main/java/week3/day1/MultiplicationTable.java
+++ java-project2/src/main/java/week3/day1/MultiplicationTable.java
@@ -1,10 +1,31 @@
 package week3.day1;
 
 public class MultiplicationTable {
-    public static void main(String[] args) {
-        for (int i = 1; i < 10; i++){
-            System.out.printf("2 * %d = %d\n", i, 2 * i);
-
+    public static void printDan(int dan){
+        for(int i = 1; i < 10; i++){
+            System.out.printf("%d * %d = %d\n", dan, i, dan * i);
         }
+    }
+
+    public static void main(String[] args) {
+        int[] dan = {2, 4, 8, 9};
+
+        for(int j = 0; j < dan.length; j++){
+            for (int i = 1; i < 10; i++){
+                System.out.printf("%d * %d = %d\n", dan[j], i, dan[j] * i);
+
+            }
+            System.out.println("========================");
+        }
+
+        System.out.println("메서드 활용 버전");
+
+        printDan(2);
+        System.out.println();
+        printDan(4);
+        System.out.println();
+        printDan(8);
+        System.out.println();
+        printDan(9);
     }
 }
