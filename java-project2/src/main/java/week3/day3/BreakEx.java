fe3da832d61cca9a9659cd301e3d63d0b1eb6895
bluelily555 <bluelily555@daum.net>
2023-05-03 15:47:02 +0900

    Java Example 반영 (week3_day3)

---
+++ java-project2/src/main/java/week3/day3/BreakEx.java
@@ -0,0 +1,12 @@
+package week3.day3;
+
+public class BreakEx {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+            if(i * i > 50){
+                break;
+            }
+        }
+    }
+}
