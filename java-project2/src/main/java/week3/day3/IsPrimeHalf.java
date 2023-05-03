027fe112c8ec7cc844a1b3cefdeaaacce1276d84
bluelily555 <bluelily555@daum.net>
2023-05-03 15:46:22 +0900

    수정 반영 (week3_day3)

--- java-project2/src/main/java/week3/day3/IsPrimeHalf.java
+++ java-project2/src/main/java/week3/day3/IsPrimeHalf.java
@@ -2,10 +2,13 @@ package week3.day3;
 
 public class IsPrimeHalf {
     public static void main(String[] args) {
-        int num = 50;
-        int answer = 0;
-        for (int i = 2; i <= num / 2; i++) {
-            if(num / i == 0) answer++;
+        int num = 53;
+        int cnt = 0;
+        for (int i = 2; i < num / 2; i++) {
+            if(num % i == 0) cnt++;
         }
+
+        if(cnt != 0) System.out.println("소수가 아닙니다.");
+        else System.out.println("소수입니다");
     }
 }
